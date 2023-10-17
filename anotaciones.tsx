2d111e1b289f5007d2938d74375d761a5c5b3087
chrisardo <chrisardorolo02@gmail.com>
2023-10-17 12:45:13 -0500

    anotaciones

--- anotaciones.tsx
+++ anotaciones.tsx
@@ -4,4 +4,14 @@ npm i pg
 npm install --save @types/pg
 npm install react-icons
 
+--next.config.js--
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ["react.semantic-ui.com"],
+    },
+};
+
+--tsconfig.json--
+"baseUrl": ".",
 */
